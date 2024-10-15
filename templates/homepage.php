b927f047f246116a5b70a4c0a7e3dcaa4dd11cb2
EnzoVandepoele <162293370+EnzoVandepoele@users.noreply.github.com>
2024-10-15 11:23:12 +0200

    Améliorations esthétiques

--- templates/homepage.php
+++ templates/homepage.php
@@ -15,13 +15,15 @@
         ?>
             <div class="news">
                 <h3>
+                    <!-- Je n'ai pas remplacé par le raccourci sans echo car cette méthode n'est pas configuré pour
+                    mon serveur php donc ça ne fonctionne pas pour moi -->
                     <?php echo htmlspecialchars($post['title']); ?>
                     <em>le <?php echo $post['french_creation_date']; ?> </em>
                 </h3>
                 <p>
                     <?php
-                    // We display the post content.
-                    echo nl2br(htmlspecialchars($post['content']));
+                        // We display the post content.
+                        echo nl2br(htmlspecialchars($post['content']));
                     ?>
                     <br />
                     <em><a href="#">Commentaires</a></em>
@@ -32,3 +34,9 @@
         ?>
     </body>
 </html>
+
+<!-- Défaut de code Question 1.4
+   utilisation de die() pour les exceptions à la place de catch (exemple : utiliser catch())
+   aucuns filtres de sécurité lors de l'ajout (exemple : utiliser filter_var())
+   mélange du code PHP et HTML dans le même code (exemple : séparer le PHP et HTML dans deux codes distinct)
+-->
