1c4f0a68ef4fe55cca5960ee2b4a60629524a42a
EnzoVandepoele <162293370+EnzoVandepoele@users.noreply.github.com>
2024-10-15 11:22:13 +0200

    Améliorations esthétiques

--- blog/index.php
+++ blog/index.php
@@ -2,10 +2,3 @@
    require('../src/model.php');
    $posts = getPosts();
    require('../templates/homepage.php');
-?>
-
-<!-- Défaut de code
-   utilisation de die() pour les exceptions à la place de catch (exemple : utiliser catch())
-   aucuns filtres de sécurité lors de l'ajout (exemple : utiliser filter_var())
-   mélange du code PHP et HTML dans le même code (exemple : séparer le PHP et HTML dans deux codes distinct)
--->
