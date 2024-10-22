dfd9d1d35bfb10d0d8e81d4ebf4456472cb07d65
Enzo VANDEPOELE <ev399356@iut21.u-bourgogne.fr>
2024-10-22 10:43:32 +0200

    Gérez les erreurs

--- blog/templates/post.php
+++ blog/templates/post.php
@@ -21,7 +21,7 @@
         </div>
 
         <h2>Commentaires</h2>
-        <form action="index.php?action=addComment&id=<? = $post['identifier'] ?>" method="post">
+        <form action="index.php?action=addComment&id=<?= $post['identifier'] ?>" method="post">
         <div>
             <label for="author">Auteur</label><br />
             <input type="text" id="author" name="author" />
