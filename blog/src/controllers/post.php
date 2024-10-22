d21c453a13f101b7ea767e738ba15ba7afa9c5b4
Enzo VANDEPOELE <ev399356@iut21.u-bourgogne.fr>
2024-10-22 11:53:22 +0200

    Tirez parti de la composition

--- blog/src/controllers/post.php
+++ blog/src/controllers/post.php
@@ -1,13 +1,18 @@
 <?php
+// src/controllers/post.php
 
-require_once('src/model/post.php');
+require_once('src/lib/database.php');
 require_once('src/model/comment.php');
+require_once('src/model/post.php');
 
 function post(string $identifier)
 {
     $postRepository = new PostRepository();
+    $commentRepository = new CommentRepository();
+    $postRepository->connection = new DatabaseConnection();
+    $commentRepository->connection = $postRepository->connection;
     $post = $postRepository->getPost($identifier);
-    $comments = getComments($identifier);
+    $comments = $commentRepository->getComments($identifier);
 
     require('templates/post.php');
 }
